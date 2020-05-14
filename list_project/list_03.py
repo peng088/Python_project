01b5a89d84363733d6689ef2becb3ba26e7afb72
peng088 <seven078@yeah.net>
2020-05-14 07:30:37 -0700

    company_ubantu

---
+++ list_project/list_03.py
@@ -0,0 +1,15 @@
+# 3-8 放眼世界
+city = ['chinese', 'shanghai', 'beijin', 'tianjin', 'xizhang']
+print(city)
+print(sorted(city))
+print(city)
+print(sorted(city,reverse = True))
+print(city)
+city.reverse()
+print(city)
+city.reverse()
+print(city)
+city.sort()
+print(city)
+city.sort(reverse = True)
+print(city)
